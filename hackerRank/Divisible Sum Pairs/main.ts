e328ad8c88626b196d49ffa607850cfd5097dbf0
erk-vural <erk.vural.dev@gmail.com>
2023-06-29 10:42:02 +0300

    Add solution

--- hackerRank/Divisible Sum Pairs/main.ts
+++ hackerRank/Divisible Sum Pairs/main.ts
@@ -35,8 +35,16 @@ function readLine(): string {
 
 function divisibleSumPairs(n: number, k: number, ar: number[]): number {
   // Write your code here
+  let result = 0;
+  for (let i = 0; i < n; i++) {
+    for (let j = i + 1; j < n; j++) {
+      if ((ar[i] + ar[j]) % k === 0) {
+        result++;
+      }
+    }
+  }
+  return result;
 }
-
 function main() {
   const ws: WriteStream = createWriteStream(process.env["OUTPUT_PATH"]);
 
